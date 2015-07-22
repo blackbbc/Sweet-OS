4254703f46e8a2f05e2e9d3af6fa41cf1ba429fe
blackbbc <505968815@qq.com>
2015-07-22 15:30:18 +0800

    remove error msg

--- fs/open.c
+++ fs/open.c
@@ -35,101 +35,106 @@ PRIVATE void new_dir_entry(struct inode * dir_inode, int inode_nr, char * filena
  *****************************************************************************/
 /**
  * Open a file and return the file descriptor.
- * 
+ *
  * @return File descriptor if successful, otherwise a negative error code.
  *****************************************************************************/
 PUBLIC int do_open()
 {
-	int fd = -1;		/* return value */
+    int fd = -1;        /* return value */
 
-	char pathname[MAX_PATH];
+    char pathname[MAX_PATH];
 
-	/* get parameters from the message */
-	int flags = fs_msg.FLAGS;	/* access mode */
-	int name_len = fs_msg.NAME_LEN;	/* length of filename */
-	int src = fs_msg.source;	/* caller proc nr. */
-	assert(name_len < MAX_PATH);
-	phys_copy((void*)va2la(TASK_FS, pathname),
-		  (void*)va2la(src, fs_msg.PATHNAME),
-		  name_len);
-	pathname[name_len] = 0;
+    /* get parameters from the message */
+    int flags = fs_msg.FLAGS;   /* access mode */
+    int name_len = fs_msg.NAME_LEN; /* length of filename */
+    int src = fs_msg.source;    /* caller proc nr. */
+    assert(name_len < MAX_PATH);
 
-	/* find a free slot in PROCESS::filp[] */
-	int i;
-	for (i = 0; i < NR_FILES; i++) {
-		if (pcaller->filp[i] == 0) {
-			fd = i;
-			break;
-		}
-	}
-	if ((fd < 0) || (fd >= NR_FILES))
-		panic("filp[] is full (PID:%d)", proc2pid(pcaller));
+    //这他妈在干啥
+    phys_copy((void*)va2la(TASK_FS, pathname),
+          (void*)va2la(src, fs_msg.PATHNAME),
+          name_len);
+    pathname[name_len] = 0;
 
-	/* find a free slot in f_desc_table[] */
-	for (i = 0; i < NR_FILE_DESC; i++)
-		if (f_desc_table[i].fd_inode == 0)
-			break;
-	if (i >= NR_FILE_DESC)
-		panic("f_desc_table[] is full (PID:%d)", proc2pid(pcaller));
+    /* find a free slot in PROCESS::filp[] */
+    int i;
+    for (i = 0; i < NR_FILES; i++) {
+        if (pcaller->filp[i] == 0) {
+            fd = i;
+            break;
+        }
+    }
 
-	int inode_nr = search_file(pathname);
+    if ((fd < 0) || (fd >= NR_FILES))
+        panic("filp[] is full (PID:%d)", proc2pid(pcaller));
 
-	struct inode * pin = 0;
-	if (flags & O_CREAT) {
-		if (inode_nr) {
-			printl("file exists.\n");
-			return -1;
-		}
-		else {
-			pin = create_file(pathname, flags);
-		}
-	}
-	else {
-		assert(flags & O_RDWR);
+    /* find a free slot in f_desc_table[] */
+    for (i = 0; i < NR_FILE_DESC; i++)
+        if (f_desc_table[i].fd_inode == 0)
+            break;
 
-		char filename[MAX_PATH];
-		struct inode * dir_inode;
-		if (strip_path(filename, pathname, &dir_inode) != 0)
-			return -1;
-		pin = get_inode(dir_inode->i_dev, inode_nr);
-	}
+    if (i >= NR_FILE_DESC)
+        panic("f_desc_table[] is full (PID:%d)", proc2pid(pcaller));
 
-	if (pin) {
-		/* connects proc with file_descriptor */
-		pcaller->filp[fd] = &f_desc_table[i];
+    int inode_nr = search_file(pathname);
 
-		/* connects file_descriptor with inode */
-		f_desc_table[i].fd_inode = pin;
+    struct inode * pin = 0;
+    if (flags & O_CREAT) {
+        if (inode_nr) {
+            //文件已存在，不要输出错误信息
+            /*printl("file exists.\n");*/
+            return -1;
+        }
+        else {
+            pin = create_file(pathname, flags);
+        }
+    }
+    else {
+        assert(flags & O_RDWR);
 
-		f_desc_table[i].fd_mode = flags;
-		/* f_desc_table[i].fd_cnt = 1; */
-		f_desc_table[i].fd_pos = 0;
+        char filename[MAX_PATH];
+        struct inode * dir_inode;
+        if (strip_path(filename, pathname, &dir_inode) != 0)
+            return -1;
+        pin = get_inode(dir_inode->i_dev, inode_nr);
+    }
 
-		int imode = pin->i_mode & I_TYPE_MASK;
+    if (pin) {
+        /* connects proc with file_descriptor */
+        pcaller->filp[fd] = &f_desc_table[i];
 
-		if (imode == I_CHAR_SPECIAL) {
-			MESSAGE driver_msg;
-			driver_msg.type = DEV_OPEN;
-			int dev = pin->i_start_sect;
-			driver_msg.DEVICE = MINOR(dev);
-			assert(MAJOR(dev) == 4);
-			assert(dd_map[MAJOR(dev)].driver_nr != INVALID_DRIVER);
-			send_recv(BOTH,
-				  dd_map[MAJOR(dev)].driver_nr,
-				  &driver_msg);
-		}
-		else if (imode == I_DIRECTORY) {
-			assert(pin->i_num == ROOT_INODE);
-		}
-		else {
-			assert(pin->i_mode == I_REGULAR);
-		}
-	}
-	else {
-		return -1;
-	}
+        /* connects file_descriptor with inode */
+        f_desc_table[i].fd_inode = pin;
 
-	return fd;
+        f_desc_table[i].fd_mode = flags;
+        /* f_desc_table[i].fd_cnt = 1; */
+        f_desc_table[i].fd_pos = 0;
+
+        int imode = pin->i_mode & I_TYPE_MASK;
+
+        if (imode == I_CHAR_SPECIAL) {
+            MESSAGE driver_msg;
+            driver_msg.type = DEV_OPEN;
+            int dev = pin->i_start_sect;
+            driver_msg.DEVICE = MINOR(dev);
+            assert(MAJOR(dev) == 4);
+            assert(dd_map[MAJOR(dev)].driver_nr != INVALID_DRIVER);
+            send_recv(BOTH,
+                  dd_map[MAJOR(dev)].driver_nr,
+                  &driver_msg);
+        }
+        else if (imode == I_DIRECTORY) {
+            assert(pin->i_num == ROOT_INODE);
+        }
+        else {
+            assert(pin->i_mode == I_REGULAR);
+        }
+    }
+    else {
+        return -1;
+    }
+
+    return fd;
 }
 
 /*****************************************************************************
@@ -142,7 +147,7 @@ PUBLIC int do_open()
  * @param[in] flags  Attribiutes of the new file
  *
  * @return           Ptr to i-node of the new file if successful, otherwise 0.
- * 
+ *
  * @see open()
  * @see do_open()
  *
@@ -150,20 +155,20 @@ PUBLIC int do_open()
  *****************************************************************************/
 PRIVATE struct inode * create_file(char * path, int flags)
 {
-	char filename[MAX_PATH];
-	struct inode * dir_inode;
-	if (strip_path(filename, path, &dir_inode) != 0)
-		return 0;
+    char filename[MAX_PATH];
+    struct inode * dir_inode;
+    if (strip_path(filename, path, &dir_inode) != 0)
+        return 0;
 
-	int inode_nr = alloc_imap_bit(dir_inode->i_dev);
-	int free_sect_nr = alloc_smap_bit(dir_inode->i_dev,
-					  NR_DEFAULT_FILE_SECTS);
-	struct inode *newino = new_inode(dir_inode->i_dev, inode_nr,
-					 free_sect_nr);
+    int inode_nr = alloc_imap_bit(dir_inode->i_dev);
+    int free_sect_nr = alloc_smap_bit(dir_inode->i_dev,
+                      NR_DEFAULT_FILE_SECTS);
+    struct inode *newino = new_inode(dir_inode->i_dev, inode_nr,
+                     free_sect_nr);
 
-	new_dir_entry(dir_inode, newino->i_num, filename);
+    new_dir_entry(dir_inode, newino->i_num, filename);
 
-	return newino;
+    return newino;
 }
 
 /*****************************************************************************
@@ -171,17 +176,17 @@ PRIVATE struct inode * create_file(char * path, int flags)
  *****************************************************************************/
 /**
  * Handle the message CLOSE.
- * 
+ *
  * @return Zero if success.
  *****************************************************************************/
 PUBLIC int do_close()
 {
-	int fd = fs_msg.FD;
-	put_inode(pcaller->filp[fd]->fd_inode);
-	pcaller->filp[fd]->fd_inode = 0;
-	pcaller->filp[fd] = 0;
+    int fd = fs_msg.FD;
+    put_inode(pcaller->filp[fd]->fd_inode);
+    pcaller->filp[fd]->fd_inode = 0;
+    pcaller->filp[fd] = 0;
 
-	return 0;
+    return 0;
 }
 
 /*****************************************************************************
@@ -189,38 +194,38 @@ PUBLIC int do_close()
  *****************************************************************************/
 /**
  * Handle the message LSEEK.
- * 
+ *
  * @return The new offset in bytes from the beginning of the file if successful,
  *         otherwise a negative number.
  *****************************************************************************/
 PUBLIC int do_lseek()
 {
-	int fd = fs_msg.FD;
-	int off = fs_msg.OFFSET;
-	int whence = fs_msg.WHENCE;
+    int fd = fs_msg.FD;
+    int off = fs_msg.OFFSET;
+    int whence = fs_msg.WHENCE;
 
-	int pos = pcaller->filp[fd]->fd_pos;
-	int f_size = pcaller->filp[fd]->fd_inode->i_size;
+    int pos = pcaller->filp[fd]->fd_pos;
+    int f_size = pcaller->filp[fd]->fd_inode->i_size;
 
-	switch (whence) {
-	case SEEK_SET:
-		pos = off;
-		break;
-	case SEEK_CUR:
-		pos += off;
-		break;
-	case SEEK_END:
-		pos = f_size + off;
-		break;
-	default:
-		return -1;
-		break;
-	}
-	if ((pos > f_size) || (pos < 0)) {
-		return -1;
-	}
-	pcaller->filp[fd]->fd_pos = pos;
-	return pos;
+    switch (whence) {
+    case SEEK_SET:
+        pos = off;
+        break;
+    case SEEK_CUR:
+        pos += off;
+        break;
+    case SEEK_END:
+        pos = f_size + off;
+        break;
+    default:
+        return -1;
+        break;
+    }
+    if ((pos > f_size) || (pos < 0)) {
+        return -1;
+    }
+    pcaller->filp[fd]->fd_pos = pos;
+    return pos;
 }
 
 /*****************************************************************************
@@ -228,43 +233,43 @@ PUBLIC int do_lseek()
  *****************************************************************************/
 /**
  * Allocate a bit in inode-map.
- * 
+ *
  * @param dev  In which device the inode-map is located.
- * 
+ *
  * @return  I-node nr.
  *****************************************************************************/
 PRIVATE int alloc_imap_bit(int dev)
 {
-	int inode_nr = 0;
-	int i, j, k;
+    int inode_nr = 0;
+    int i, j, k;
 
-	int imap_blk0_nr = 1 + 1; /* 1 boot sector & 1 super block */
-	struct super_block * sb = get_super_block(dev);
+    int imap_blk0_nr = 1 + 1; /* 1 boot sector & 1 super block */
+    struct super_block * sb = get_super_block(dev);
 
-	for (i = 0; i < sb->nr_imap_sects; i++) {
-		RD_SECT(dev, imap_blk0_nr + i);
+    for (i = 0; i < sb->nr_imap_sects; i++) {
+        RD_SECT(dev, imap_blk0_nr + i);
 
-		for (j = 0; j < SECTOR_SIZE; j++) {
-			/* skip `11111111' bytes */
-			if (fsbuf[j] == 0xFF)
-				continue;
-			/* skip `1' bits */
-			for (k = 0; ((fsbuf[j] >> k) & 1) != 0; k++) {}
-			/* i: sector index; j: byte index; k: bit index */
-			inode_nr = (i * SECTOR_SIZE + j) * 8 + k;
-			fsbuf[j] |= (1 << k);
-			/* write the bit to imap */
-			WR_SECT(dev, imap_blk0_nr + i);
-			break;
-		}
+        for (j = 0; j < SECTOR_SIZE; j++) {
+            /* skip `11111111' bytes */
+            if (fsbuf[j] == 0xFF)
+                continue;
+            /* skip `1' bits */
+            for (k = 0; ((fsbuf[j] >> k) & 1) != 0; k++) {}
+            /* i: sector index; j: byte index; k: bit index */
+            inode_nr = (i * SECTOR_SIZE + j) * 8 + k;
+            fsbuf[j] |= (1 << k);
+            /* write the bit to imap */
+            WR_SECT(dev, imap_blk0_nr + i);
+            break;
+        }
 
-		return inode_nr;
-	}
+        return inode_nr;
+    }
 
-	/* no free bit in imap */
-	panic("inode-map is probably full.\n");
+    /* no free bit in imap */
+    panic("inode-map is probably full.\n");
 
-	return 0;
+    return 0;
 }
 
 /*****************************************************************************
@@ -272,58 +277,58 @@ PRIVATE int alloc_imap_bit(int dev)
  *****************************************************************************/
 /**
  * Allocate a bit in sector-map.
- * 
+ *
  * @param dev  In which device the sector-map is located.
  * @param nr_sects_to_alloc  How many sectors are allocated.
- * 
+ *
  * @return  The 1st sector nr allocated.
  *****************************************************************************/
 PRIVATE int alloc_smap_bit(int dev, int nr_sects_to_alloc)
 {
-	/* int nr_sects_to_alloc = NR_DEFAULT_FILE_SECTS; */
+    /* int nr_sects_to_alloc = NR_DEFAULT_FILE_SECTS; */
 
-	int i; /* sector index */
-	int j; /* byte index */
-	int k; /* bit index */
+    int i; /* sector index */
+    int j; /* byte index */
+    int k; /* bit index */
 
-	struct super_block * sb = get_super_block(dev);
+    struct super_block * sb = get_super_block(dev);
 
-	int smap_blk0_nr = 1 + 1 + sb->nr_imap_sects;
-	int free_sect_nr = 0;
+    int smap_blk0_nr = 1 + 1 + sb->nr_imap_sects;
+    int free_sect_nr = 0;
 
-	for (i = 0; i < sb->nr_smap_sects; i++) { /* smap_blk0_nr + i :
-						     current sect nr. */
-		RD_SECT(dev, smap_blk0_nr + i);
+    for (i = 0; i < sb->nr_smap_sects; i++) { /* smap_blk0_nr + i :
+                             current sect nr. */
+        RD_SECT(dev, smap_blk0_nr + i);
 
-		/* byte offset in current sect */
-		for (j = 0; j < SECTOR_SIZE && nr_sects_to_alloc > 0; j++) {
-			k = 0;
-			if (!free_sect_nr) {
-				/* loop until a free bit is found */
-				if (fsbuf[j] == 0xFF) continue;
-				for (; ((fsbuf[j] >> k) & 1) != 0; k++) {}
-				free_sect_nr = (i * SECTOR_SIZE + j) * 8 +
-					k - 1 + sb->n_1st_sect;
-			}
+        /* byte offset in current sect */
+        for (j = 0; j < SECTOR_SIZE && nr_sects_to_alloc > 0; j++) {
+            k = 0;
+            if (!free_sect_nr) {
+                /* loop until a free bit is found */
+                if (fsbuf[j] == 0xFF) continue;
+                for (; ((fsbuf[j] >> k) & 1) != 0; k++) {}
+                free_sect_nr = (i * SECTOR_SIZE + j) * 8 +
+                    k - 1 + sb->n_1st_sect;
+            }
 
-			for (; k < 8; k++) { /* repeat till enough bits are set */
-				assert(((fsbuf[j] >> k) & 1) == 0);
-				fsbuf[j] |= (1 << k);
-				if (--nr_sects_to_alloc == 0)
-					break;
-			}
-		}
+            for (; k < 8; k++) { /* repeat till enough bits are set */
+                assert(((fsbuf[j] >> k) & 1) == 0);
+                fsbuf[j] |= (1 << k);
+                if (--nr_sects_to_alloc == 0)
+                    break;
+            }
+        }
 
-		if (free_sect_nr) /* free bit found, write the bits to smap */
-			WR_SECT(dev, smap_blk0_nr + i);
+        if (free_sect_nr) /* free bit found, write the bits to smap */
+            WR_SECT(dev, smap_blk0_nr + i);
 
-		if (nr_sects_to_alloc == 0)
-			break;
-	}
+        if (nr_sects_to_alloc == 0)
+            break;
+    }
 
-	assert(nr_sects_to_alloc == 0);
+    assert(nr_sects_to_alloc == 0);
 
-	return free_sect_nr;
+    return free_sect_nr;
 }
 
 /*****************************************************************************
@@ -331,30 +336,30 @@ PRIVATE int alloc_smap_bit(int dev, int nr_sects_to_alloc)
  *****************************************************************************/
 /**
  * Generate a new i-node and write it to disk.
- * 
+ *
  * @param dev  Home device of the i-node.
  * @param inode_nr  I-node nr.
  * @param start_sect  Start sector of the file pointed by the new i-node.
- * 
+ *
  * @return  Ptr of the new i-node.
  *****************************************************************************/
 PRIVATE struct inode * new_inode(int dev, int inode_nr, int start_sect)
 {
-	struct inode * new_inode = get_inode(dev, inode_nr);
+    struct inode * new_inode = get_inode(dev, inode_nr);
 
-	new_inode->i_mode = I_REGULAR;
-	new_inode->i_size = 0;
-	new_inode->i_start_sect = start_sect;
-	new_inode->i_nr_sects = NR_DEFAULT_FILE_SECTS;
+    new_inode->i_mode = I_REGULAR;
+    new_inode->i_size = 0;
+    new_inode->i_start_sect = start_sect;
+    new_inode->i_nr_sects = NR_DEFAULT_FILE_SECTS;
 
-	new_inode->i_dev = dev;
-	new_inode->i_cnt = 1;
-	new_inode->i_num = inode_nr;
+    new_inode->i_dev = dev;
+    new_inode->i_cnt = 1;
+    new_inode->i_num = inode_nr;
 
-	/* write to the inode array */
-	sync_inode(new_inode);
+    /* write to the inode array */
+    sync_inode(new_inode);
 
-	return new_inode;
+    return new_inode;
 }
 
 /*****************************************************************************
@@ -362,55 +367,55 @@ PRIVATE struct inode * new_inode(int dev, int inode_nr, int start_sect)
  *****************************************************************************/
 /**
  * Write a new entry into the directory.
- * 
+ *
  * @param dir_inode  I-node of the directory.
  * @param inode_nr   I-node nr of the new file.
  * @param filename   Filename of the new file.
  *****************************************************************************/
 PRIVATE void new_dir_entry(struct inode *dir_inode,int inode_nr,char *filename)
 {
-	/* write the dir_entry */
-	int dir_blk0_nr = dir_inode->i_start_sect;
-	int nr_dir_blks = (dir_inode->i_size + SECTOR_SIZE) / SECTOR_SIZE;
-	int nr_dir_entries =
-		dir_inode->i_size / DIR_ENTRY_SIZE; /**
-						     * including unused slots
-						     * (the file has been
-						     * deleted but the slot
-						     * is still there)
-						     */
-	int m = 0;
-	struct dir_entry * pde;
-	struct dir_entry * new_de = 0;
+    /* write the dir_entry */
+    int dir_blk0_nr = dir_inode->i_start_sect;
+    int nr_dir_blks = (dir_inode->i_size + SECTOR_SIZE) / SECTOR_SIZE;
+    int nr_dir_entries =
+        dir_inode->i_size / DIR_ENTRY_SIZE; /**
+                             * including unused slots
+                             * (the file has been
+                             * deleted but the slot
+                             * is still there)
+                             */
+    int m = 0;
+    struct dir_entry * pde;
+    struct dir_entry * new_de = 0;
 
-	int i, j;
-	for (i = 0; i < nr_dir_blks; i++) {
-		RD_SECT(dir_inode->i_dev, dir_blk0_nr + i);
+    int i, j;
+    for (i = 0; i < nr_dir_blks; i++) {
+        RD_SECT(dir_inode->i_dev, dir_blk0_nr + i);
 
-		pde = (struct dir_entry *)fsbuf;
-		for (j = 0; j < SECTOR_SIZE / DIR_ENTRY_SIZE; j++,pde++) {
-			if (++m > nr_dir_entries)
-				break;
+        pde = (struct dir_entry *)fsbuf;
+        for (j = 0; j < SECTOR_SIZE / DIR_ENTRY_SIZE; j++,pde++) {
+            if (++m > nr_dir_entries)
+                break;
 
-			if (pde->inode_nr == 0) { /* it's a free slot */
-				new_de = pde;
-				break;
-			}
-		}
-		if (m > nr_dir_entries ||/* all entries have been iterated or */
-		    new_de)              /* free slot is found */
-			break;
-	}
-	if (!new_de) { /* reached the end of the dir */
-		new_de = pde;
-		dir_inode->i_size += DIR_ENTRY_SIZE;
-	}
-	new_de->inode_nr = inode_nr;
-	strcpy(new_de->name, filename);
+            if (pde->inode_nr == 0) { /* it's a free slot */
+                new_de = pde;
+                break;
+            }
+        }
+        if (m > nr_dir_entries ||/* all entries have been iterated or */
+            new_de)              /* free slot is found */
+            break;
+    }
+    if (!new_de) { /* reached the end of the dir */
+        new_de = pde;
+        dir_inode->i_size += DIR_ENTRY_SIZE;
+    }
+    new_de->inode_nr = inode_nr;
+    strcpy(new_de->name, filename);
 
-	/* write dir block -- ROOT dir block */
-	WR_SECT(dir_inode->i_dev, dir_blk0_nr + i);
+    /* write dir block -- ROOT dir block */
+    WR_SECT(dir_inode->i_dev, dir_blk0_nr + i);
 
-	/* update dir inode */
-	sync_inode(dir_inode);
+    /* update dir inode */
+    sync_inode(dir_inode);
 }
